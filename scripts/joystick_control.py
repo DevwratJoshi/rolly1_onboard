0653bae3b9cec75ec22780cd4c94304010fe3bf7
Devwrat Joshi <devwratjoshi1234@gmail.com>
2021-03-03 13:47:10 +0000

    Add buggy code to write to serial

--- scripts/joystick_control.py
+++ scripts/joystick_control.py
@@ -1,24 +1,37 @@
 import socket
 import serial
 from time import sleep
-HOST = ''  # Standard loopback interface address (localhost)
+HOST = '' 
 PORT = 2000        # Port to listen on (non-privileged ports are > 1023)
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        print("Bound port")
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if data:
-                    print(data.decode("utf-8"))
-                    if(data.decode('utf-8')[0] == 'x'):
-                        conn.sendall(b'Ending')
-                        #Close the connection
-                        break
-                    conn.sendall(data)
-            print("Connection broken\n")
-            sleep(1.0)
+def get_command(d):
+    if d == 'rflf':
+        return "forward"
+    elif d == 'rblb':
+        return "backward"
+    else:
+        return "stop"
+with serial.Serial(port='/dev/ttyUSB0', baudrate=9600) as ser:
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            print("Bound port")
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if data:
+                        data = data.decode('utf-8')
+
+                        if(data[0] == 'x'):
+                            conn.sendall(b'Ending')
+                            #Close the connection
+                            break
+                        else:
+                            command = get_command(data) 
+                            print(command)
+                            ser.write(command.encode('utf-8'))
+                            print(ser.readline())
+                print("Connection broken\n")
+                sleep(1.0)
