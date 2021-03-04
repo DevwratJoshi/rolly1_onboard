4969771971c3450dc206b74e57bcc8e0af98df72
Devwrat Joshi <devwratjoshi1234@gmail.com>
2021-03-04 12:01:19 +0900

    FIXUP: serial debugging 2

--- scripts/serial_test.py
+++ scripts/serial_test.py
@@ -5,13 +5,19 @@ HOST = ''
 PORT = 2000        # Port to listen on (non-privileged ports are > 1023)
 def get_command(d):
     if d == 'rflf':
-        return "forward"
+        return "forward\n"
     elif d == 'rblb':
-        return "backward"
+        return "backward\n"
     else:
-        return "stop"
-with serial.Serial(port='/dev/ttyUSB0', baudrate=9600) as ser:
+        return "stop\n"
+def main():
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+    ser.flush()
     data = 'rff'
-    command = get_command(data) 
-    ser.write(command.encode('utf-8'))
-    print(ser.readline())
+    while True: 
+        command = get_command(data) 
+        ser.write(command.encode('utf-8'))
+        print(ser.readline())
+            
+if __name__ == '__main__':
+    main()
