957f19cef6eb621f34bc4c2ae02dc7e382e3bc3a
Devwrat Joshi <devwratjoshi1234@gmail.com>
2021-09-23 01:41:17 +0100

    Clarify code by defining motor duty constants as enums

--- scripts/distance_sensor_handler.py
+++ scripts/distance_sensor_handler.py
@@ -3,12 +3,23 @@ import time
 from threading import Thread, Lock
 import math
 import numpy as np
-class DistanceSensorHandler:
+import enum
+
+class CONSTS(enum.Enum):
+    maxServoDuty = 12.5 # The max duty cycle for the servo
+    minServoDuty = 2.5  # The min duty cycle for the servo
+class SensorTurretHandler:
     '''
-    A class that handles the rolly distance sensors
-    These sensors consist of a HCSR04 ultrasound sensor mounted on a SG90 servo motor
+    A class that handles rolly's sensor turret
+    This consists of a number of sensors mounted on a servo motor turret
+    (For now, only a distance sensor)
+
+    On instantization, this class will:
+    1) Set the servo motor to the initial position 
+    2) Rotate the servo turret back and forth between 0 and 180 degrees at the prescribed step resolution and freqency
+    3) Take sensor readings at every step
     '''
-    def __init__(self, servoPin=25, ultraTrigger=18, ultraEcho=24, scanFreq=1.0, scanRes=0.0174533):
+    def __init__(self, servoPin=25, ultraTrigger=18, ultraEcho=24, scanFreq=1.5, scanRes=0.0174533):
         '''
         @param servoPin The servo input pin (GPIO pin number)
         @param ultraTrigger The HCSR04 trigger pin (GPIO pin number)
@@ -30,16 +41,20 @@ class DistanceSensorHandler:
         self.thread = Thread(target=self.scanDistances)
         self.thread.start()
         self.lastDutyCycle = 0.
-        self.step = (self.scanRes/math.pi) * (5)
+        self.step = (self.scanRes/math.pi) * (CONSTS.maxServoDuty.value - CONSTS.minServoDuty.value)
         # The time per step in milliseconds
         self.stepTime = (1.0/(2*scanFreq)) * (scanRes/math.pi) * 1000
-    def reset_motor(self):
-        self.servo.ChangeDutyCycle(2.5)
-        self.lastDutyCycle = 5.
-        time.sleep(0.5)
-        return
+        print(self.stepTime)
+        self.reset_motor()
+    
     def __del__(self):
         self.stop()
+
+    def reset_motor(self):
+        self.servo.ChangeDutyCycle(CONSTS.minServoDuty.value)
+        self.lastDutyCycle = CONSTS.minServoDuty.value
+        time.sleep(0.5)
+        return
 
     def measureDistance(self):
         time.sleep(0.01)
@@ -51,7 +66,7 @@ class DistanceSensorHandler:
         '''
         # The direction the motor is turning in. +1 for anti-clockwise and -1 for clockwise
         direction = 1
-        nextCycle = 5
+        nextCycle = CONSTS.minServoDuty.value
         while 1:
             # Check if program is to stopped
             if(self.stop_requested):
@@ -67,17 +82,18 @@ class DistanceSensorHandler:
 
             nextCycle = self.lastDutyCycle + direction*self.step
             
-            if nextCycle > 12.5:
+            if nextCycle > CONSTS.maxServoDuty.value:
                 direction = -1
-                nextCycle = 12.5
+                nextCycle = CONSTS.maxServoDuty.value
 
-            elif nextCycle < 2.5:
+            elif nextCycle < CONSTS.minServoDuty.value:
                 direction = 1
-                nextcycle = 2.5
+                nextcycle = CONSTS.minServoDuty.value
             
             # Wait the appropriate amount of time before the next loop cycle
             curr_time = time.time() * 1000
             # Rather than wait a set amount of time, we wait till the time taken by this step is equal to or greater than the time per step
+            print(curr_time - start_time)
             while curr_time - start_time < self.stepTime:
                 time.sleep(0.0005) #The likely time per step is about 0.003 seconds for a 1 rps rotation freq
                 curr_time = time.time()*1000
@@ -90,4 +106,4 @@ class DistanceSensorHandler:
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    d=DistanceSensorHandler()
+    d=SensorTurretHandler(scanRes=0.0873)
