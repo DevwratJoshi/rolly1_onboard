41c86af5295798596704b09f38b1d884df908dff
Devwrat Joshi <devwratjoshi1234@gmail.com>
2021-12-31 10:34:28 +0000

    Edit to account for arduino communication protocol

--- scripts/serial_test.py
+++ scripts/serial_test.py
@@ -1,23 +1,29 @@
-import socket
+#!/usr/bin/env python3
 import serial
 from time import sleep
 HOST = '' 
 PORT = 2000        # Port to listen on (non-privileged ports are > 1023)
 def get_command(d):
-    if d == 'rflf':
-        return "forward\n"
-    elif d == 'rblb':
-        return "backward\n"
-    else:
-        return "stop\n"
+        if d:
+            return "<1.0,1.0>"
+        return "<0.0,0.0>"
 def main():
-    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
     ser.flush()
-    data = 'rff'
+    data = ''
     while True: 
-        command = get_command(data) 
+        command = get_command(True) 
+        print(f"Command is {command}")
         ser.write(command.encode('utf-8'))
+        print("Got from arduino: ")
         print(ser.readline())
+        sleep(5)
+        command = get_command(False) 
+        print(f"Command is {command}")
+        ser.write(command.encode('utf-8'))
+        print("Got from arduino: ")
+        print(ser.readline())
+        sleep(5)
             
 if __name__ == '__main__':
     main()
